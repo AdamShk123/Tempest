2266981ddd32023a828841de4a7744d4f5f4abd1
AdamShk123 <adamshkolnik5@gmail.com>
2023-03-20 21:10:05 -0500

    created window class

---
+++ include/window.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <SDL2/SDL.h>
+
+#include <SDL2/SDL_image.h>
+
+#include <iostream>
+
+#include "media.hpp"
+
+// screen dimension constants
+const int SCREEN_WIDTH = 1920;
+const int SCREEN_HEIGHT = 1080;
+
+class Window
+{
+    private:
+        SDL_Window *window;
+        SDL_Renderer *renderer;
+
+    public:
+        Window();
+        ~Window();
+
+        SDL_Window *getWindow();
+        SDL_Renderer *getRenderer();
+
+        void initWindow();
+        void close();
+
+        void clear();
+        void draw(SDL_Texture *texture, SDL_Rect *rect);
+        void render();
+
+};
---
+++ src/window.cpp
@@ -0,0 +1,89 @@
+#include "../include/window.hpp"
+
+Window::Window()
+{
+    initWindow();
+}
+
+Window::~Window()
+{
+    close();
+}
+
+SDL_Window *Window::getWindow()
+{
+    return window;
+}
+
+SDL_Renderer *Window::getRenderer()
+{
+    return renderer;
+}
+
+void Window::clear()
+{
+    //Clear screen
+    SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+    SDL_RenderClear(renderer);
+}
+
+void Window::draw(SDL_Texture *texture, SDL_Rect *rect)
+{
+    SDL_RenderCopy(renderer, texture, NULL, rect);
+}
+
+void Window::render()
+{
+    //Update screen
+	SDL_RenderPresent(renderer);
+}
+
+void Window::initWindow()
+{
+    SDL_Window *window = NULL;
+
+    // init SDL
+    if(SDL_Init(SDL_INIT_VIDEO))
+    {
+        std::cout << "SDL could not initialize! SDL Error: " << SDL_GetError() << "\n";
+    }
+    else
+    {
+        // create window
+        window = SDL_CreateWindow("Tempest", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if(window == NULL)
+        {
+            std::cout << "Window could not be created! SDL Error: " << SDL_GetError() << "\n";
+        }
+        else
+        {
+            //Create renderer for window
+            renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+            if(renderer == NULL)
+            {
+                std::cout << "Renderer could not be created! SDL Error: " << SDL_GetError() << "\n";
+            }
+            else
+            {
+                int imgFlags = IMG_INIT_JPG;
+                if(!(IMG_Init(imgFlags) & imgFlags))
+                {
+                    std::cout << "SDL_image could not initialize! SDL_image Error: " << IMG_GetError() << "\n";
+                }
+            }
+        }
+    }
+}
+
+void Window::close()
+{
+	//Destroy window	
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+
+	//Quit SDL subsystems
+	IMG_Quit();
+	SDL_Quit();
+}
+
+
