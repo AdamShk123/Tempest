7c45931045b594139a7e2ffb349b3f958c37a8a5
AdamShk123 <adamshkolnik5@gmail.com>
2023-03-20 21:08:07 -0500

    placeholder components

---
+++ include/components.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+struct Texture
+{
+    SDL_Texture *t;
+};
+
+struct Position
+{
+    int x;
+    int y;
+};
+
+struct Velocity
+{
+    float x;
+    float y;
+};
